9d3f504dd8852983bb3f2d7bd7e967fad2ce171a
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-12-08 17:28:32 +0100

    change procfile

--- index.js
+++ index.js
@@ -17,4 +17,5 @@ app.use(bodyParser.urlencoded({extended: true})) //accepts url-encoded params
 TodoController(app)
 
 app.listen(port)
+console.log(process.env.PORT)
 console.log(`App is running and listening to port: ${process.env.PORT? process.env.PORT: 3000}`)
